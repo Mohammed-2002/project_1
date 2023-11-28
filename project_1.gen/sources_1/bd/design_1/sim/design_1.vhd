18644133e68f447a1914a3e13e65f32afe6eaab6
Mohammed Albayyouk <mohammedmk.albayyuk@student.uhasselt.be>
2023-11-28 12:39:23 +0100

    first commit

---
+++ project_1.gen/sources_1/bd/design_1/sim/design_1.vhd
@@ -0,0 +1,10 @@
+--Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+--Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2023.2 (win64) Build 4029153 Fri Oct 13 20:14:34 MDT 2023
+--Date        : Tue Nov 21 14:41:57 2023
+--Host        : DESKTOP-O3UC2KR running 64-bit major release  (build 9200)
+--Command     : generate_target design_1.bd
+--Design      : design_1
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
